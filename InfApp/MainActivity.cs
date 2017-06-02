729af8adae7b886a41441f7a82e1c8546b34a3eb
Stractus <charles-marchand@hotmail.fr>
2017-06-02 11:03:45 +0200

    importing VS project

---
+++ InfApp/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.Res;
+using static Android.Views.View;
+using Android.Views;
+using InfApp;
+
+namespace Inforges_Application
+{
+    [Activity(Label = "Inforges Application", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+
+        Button MainToIn;
+        Button MainToPick;
+        Button MainToRelocate;
+        Button MainToToFact;
+
+        Button InBack;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //Set our view from the "main" layout resource
+            Activate_main();
+        }
+
+        void Activate_main()
+        {
+
+            SetContentView(Resource.Layout.Main);
+            MainToIn = FindViewById<Button>(Resource.Id.MainToInButton);
+            MainToPick = FindViewById<Button>(Resource.Id.MainToPickButton);
+            MainToRelocate = FindViewById<Button>(Resource.Id.MainToRelocateButton);
+            MainToToFact = FindViewById<Button>(Resource.Id.MainToToFactButton);
+
+            MainToIn.Click += delegate
+            {
+                Activate_in();
+
+            };
+
+
+        }
+
+        void Activate_in()
+        {
+            SetContentView(Resource.Layout.InMainWindow);
+            InBack = FindViewById<Button>(Resource.Id.InToMain);
+            InBack.Click += delegate
+            {
+                Activate_main();
+            };
+        }
+    }
+
+}
+
